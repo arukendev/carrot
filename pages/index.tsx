bef881fe624985ad94c12f474e8f132e5027bc2e
arukendev <iamjaehyun4@gmail.com>
2022-10-31 21:06:19 +0900

    make first box

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,40 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-red-500">
-      <h1 className="text-blue-500">it works!</h1>
+    <div className="bg-slate-400 py-20 px-10 grid gap-10">
+      <div className="bg-white p-6 rounded-3xl shadow-xl">
+        <span className="font-semibold text-3xl">Select Item</span>
+        <div className="flex justify-between my-2">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="text-gray-500">Tooly Table</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+          <span>Total</span>
+          <span className="font-semibold">$10</span>
+        </div>
+        <div className="mt-5 bg-blue-500 text-white text-center rounded-xl w-2/4 mx-auto p-3">
+          Checkout
+        </div>
+      </div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl">
+        <span>Profile</span>
+        <div>
+          <span>Orders</span>
+        </div>
+        <div>
+          <span>Spent</span>
+        </div>
+        <div>
+          <span>Tony Molly</span>
+          <span>New York, Usa</span>
+        </div>
+      </div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
     </div>
   );
 };
